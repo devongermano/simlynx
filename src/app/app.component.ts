19cb2b777a0f3ca10211338a431be54f41bd9c70
Devon Germano <devon.m.gemrano@gmail.com>
2018-07-20 12:56:47 -0500

    Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as animateOnScroll from 'aos';
 import {MnFullpageOptions, MnFullpageService} from 'ngx-fullpage';
 
 export const aos = animateOnScroll;
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +17,16 @@ export class AppComponent {
 
   @Input() public options: MnFullpageOptions = new MnFullpageOptions({
     controlArrows: false,
-    scrollingSpeed: 1000,
+    // scrollingSpeed: 1000,
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    scrollBar: true,
+    menu: '.header',
+    verticalCentered: false,
+    dragAndMove: true,
+    navigation: true,
 
-    menu: '.menu',
-    css3: true,
+    // menu: '.menu',
+    // css3: true,
     anchors: [
       'menuAnchor1', 'menuAnchor2', 'menuAnchor3',
       'menuAnchor4', 'menuAnchor5', 'menuAnchor6'
