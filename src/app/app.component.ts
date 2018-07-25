3d1a03501fe5489de2d6ff285f5ebcc946335259
Devon Germano <devon.m.gemrano@gmail.com>
2018-07-25 16:27:12 -0500

    Lower layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {MnFullpageOptions, MnFullpageService} from 'ngx-fullpage';
 import * as Rellax from 'rellax';
 import * as animateOnScroll from 'aos';
 export const aos = animateOnScroll;
-
+declare let SmoothScroll: any;
 
 @Component({
   selector: 'app-root',
@@ -16,25 +16,31 @@ export class AppComponent implements AfterViewInit {
   title = 'app';
 
 
-  @Input() public options: MnFullpageOptions = new MnFullpageOptions({
-    controlArrows: false,
-    // scrollingSpeed: 1000,
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    scrollBar: true,
-    menu: '.header',
-    verticalCentered: false,
-    navigation: true,
-
-    // menu: '.menu',
-    // css3: true,
-    anchors: [
-      'Home', 'Gist', 'Yeah', 'Flavors',
-      'Meat', 'End'
-    ]
-  });
+  // @Input() public options: MnFullpageOptions = new MnFullpageOptions({
+  //   controlArrows: false,
+  //   // scrollingSpeed: 1000,
+  //   licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  //   scrollBar: true,
+  //   menu: '.header',
+  //   verticalCentered: false,
+  //   navigation: true,
+  //   autoScrolling: false,
+  //
+  //   // menu: '.menu',
+  //   // css3: true,
+  //   anchors: [
+  //     'Home', 'Gist', 'Yeah', 'Flavors',
+  //     'Meat', 'End'
+  //   ]
+  // });
 
   constructor(public fullpage: MnFullpageService) {
     aos.init();
+    SmoothScroll({
+      // Scrolling Core
+      animationTime: 350, // [ms]
+      stepSize: 100 // [px]
+    });
   }
 
   ngAfterViewInit() {
